a2f504b4667b991f77ce09f6a7af7703092cddd8
JoannaDraganek <98357183+JoannaDraganek@users.noreply.github.com>
2022-12-11 21:15:24 +0100

    Added function to JS.

--- JS/script.js
+++ JS/script.js
@@ -1,52 +1,56 @@
-let formElement = document.querySelector(".js-form");
-let amountElement = document.querySelector(".js-amount");
-let exchangeElement = document.querySelector(".js-exchange");
-let resultElement = document.querySelector(".js-result");
+{  
+    const currencyConversion = (amount, exchange) => {
+        const eurExchangeRate = 4.69;
+        const usdExchangeRate = 4.45;
+        const gbpExchangeRate = 5.46;
+        switch (exchange) {
+            case "fromPlnToEur":
+                currency = "EUR";
+            return amount / eurExchangeRate;
 
-
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    let amount = +amountElement.value;
-    let exchange = exchangeElement.value;
-    let eurExchangeRate = 4.69;
-    let usdExchangeRate = 4.45;
-    let gbpExchangeRate = 5.46;
-
-    let result;
-    let currency;
-
-    switch (exchange) {
-        case "fromPlnToEur":
-            result = amount / eurExchangeRate;
-            currency = "EUR";
-            break;
-
-        case "fromPlnToUsd":
-            result = amount / usdExchangeRate;
-            currency = "USD";
-            break;
-
-        case "fromPlnToGbp":
-            result = amount / gbpExchangeRate;
-            currency = "GBP";
-            break;
-
-        case "fromEurToPln":
-            result = amount * eurExchangeRate;
-            currency = "PLN";
-            break;
-
-        case "fromUsdToPln":
-            result = amount * usdExchangeRate;
-            currency = "PLN";
-            break;
-
-        case "fromGbpToPln":
-            result = amount * gbpExchangeRate;
-            currency = "PLN";
-            break;
+            case "fromPlnToUsd":
+                currency = "USD";
+                return amount / usdExchangeRate;
+    
+            case "fromPlnToGbp":
+                currency = "GBP";
+                return amount / gbpExchangeRate;
+    
+            case "fromEurToPln":
+                currency = "PLN";
+                return amount * eurExchangeRate;
+    
+            case "fromUsdToPln":
+                currency = "PLN";
+                return amount * usdExchangeRate;
+    
+            case "fromGbpToPln":
+                currency = "PLN";
+                return amount * gbpExchangeRate;
+        } 
     }
 
+const updateResultText = (result, currency) => {
+    const resultElement = document.querySelector(".js-result");
     resultElement.innerText = `${result.toFixed(2)} ${currency}`;
-});
+};
+
+    const onFormSubit = (event) => {
+        event.preventDefault();
+        
+        const amountElement = document.querySelector(".js-amount");
+        const exchangeElement = document.querySelector(".js-exchange");
+
+        const amount = +amountElement.value;
+        const exchange = exchangeElement.value;
+           
+        const result = currencyConversion(amount, exchange);
+        updateResultText(result, currency);
+    };
+
+    const init = () => {
+        const formElement = document.querySelector(".js-form");
+        formElement.addEventListener("submit", onFormSubit);
+    };
+   init();
+}
