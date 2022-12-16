9c934e38af5e68c53ad01edcb9c08eff571516d2
JoannaDraganek <98357183+JoannaDraganek@users.noreply.github.com>
2022-12-16 19:59:48 +0100

    Improved function updateResultText. Fixed the name of the currency conversion function.

--- JS/script.js
+++ JS/script.js
@@ -1,56 +1,57 @@
-{  
-    const currencyConversion = (amount, exchange) => {
+{
+    const calculateResult = (amount, exchange) => {
         const eurExchangeRate = 4.69;
         const usdExchangeRate = 4.45;
         const gbpExchangeRate = 5.46;
         switch (exchange) {
             case "fromPlnToEur":
                 currency = "EUR";
-            return amount / eurExchangeRate;
+                return amount / eurExchangeRate;
 
             case "fromPlnToUsd":
                 currency = "USD";
                 return amount / usdExchangeRate;
-    
+
             case "fromPlnToGbp":
                 currency = "GBP";
                 return amount / gbpExchangeRate;
-    
+
             case "fromEurToPln":
                 currency = "PLN";
                 return amount * eurExchangeRate;
-    
+
             case "fromUsdToPln":
                 currency = "PLN";
                 return amount * usdExchangeRate;
-    
+
             case "fromGbpToPln":
                 currency = "PLN";
                 return amount * gbpExchangeRate;
-        } 
+        }
     }
-
-const updateResultText = (result, currency) => {
-    const resultElement = document.querySelector(".js-result");
-    resultElement.innerText = `${result.toFixed(2)} ${currency}`;
-};
 
     const onFormSubit = (event) => {
         event.preventDefault();
-        
+
         const amountElement = document.querySelector(".js-amount");
         const exchangeElement = document.querySelector(".js-exchange");
 
         const amount = +amountElement.value;
         const exchange = exchangeElement.value;
-           
-        const result = currencyConversion(amount, exchange);
+
+        const result = calculateResult(amount, exchange);
         updateResultText(result, currency);
+    };
+
+    const updateResultText = (text) => {
+        const resultElement = document.querySelector(".js-result");
+        resultElement.innerText = text;
     };
 
     const init = () => {
         const formElement = document.querySelector(".js-form");
         formElement.addEventListener("submit", onFormSubit);
+        updateResultText(`${result.toFixed(2)} ${currency}`);
     };
-   init();
+    init();
 }
